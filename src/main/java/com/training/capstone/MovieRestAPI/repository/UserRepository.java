b69e4aa7954c4a5939abfee0fb8c9716d3297e57
17BIT050 <ctpraveenkumar36@gmail.com>
2025-01-05 13:32:34 +0530

    Added Spring security and updated logic for persistance_v2

---
+++ src/main/java/com/training/capstone/MovieRestAPI/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.training.capstone.MovieRestAPI.repository;
+
+
+
+
+import com.training.capstone.MovieRestAPI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
+
